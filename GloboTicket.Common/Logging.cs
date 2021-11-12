3cc3746fc36b8959cb9de7f9032019e7997b177b
Seok Chan Chung <csckhw@gmail.com>
2021-11-12 14:05:51 -0500

    init

---
+++ GloboTicket.Common/Logging.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using Serilog.Sinks.Elasticsearch;
+
+namespace GloboTicket.Common
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger =>
+            (hostingContext, loggerConfiguration) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                loggerConfiguration.MinimumLevel.Information()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                    .WriteTo.Console();
+
+                var elasticUrl = hostingContext.Configuration.GetValue<string>("Logging:ElasticUrl");
+                if (!string.IsNullOrEmpty(elasticUrl))
+                {
+                    loggerConfiguration.WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(new Uri(elasticUrl))
+                        {
+                            AutoRegisterTemplate = true,
+                            AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                            IndexFormat = "globoticket-logs-{0:yyyy.MM.dd}",
+                            MinimumLogEventLevel = LogEventLevel.Debug
+                        });
+                }
+
+                if (hostingContext.HostingEnvironment.IsDevelopment())
+                {
+                    loggerConfiguration.MinimumLevel.Override("GloboTicket", LogEventLevel.Debug);
+                }
+            };
+    }
+}
